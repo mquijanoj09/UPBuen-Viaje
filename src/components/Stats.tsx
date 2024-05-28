41d7a908e994b75f77a7ffbe56c61bba8c184b53
mquijanoj09 <mquijanoj@hotmail.com>
2024-05-27 20:40:10 -0500

    Feat: Stats added

--- src/components/Stats.tsx
+++ src/components/Stats.tsx
@@ -1,37 +1,87 @@
 "use client";
-
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Chart } from "react-google-charts";
 
 export default function Stats() {
+  const [user, setUser] = useState<any>({});
+  const [viajes, setViajes] = useState<any[]>([]);
+  const requestedRides = viajes.filter(
+    (viaje) => user && viaje.users?.includes(user.id)
+  );
+  const tripsPerDay = requestedRides.reduce((acc, ride) => {
+    const date = new Date(ride.date);
+    const day = date.toLocaleDateString("es-ES", { weekday: "long" });
+    if (acc[day]) {
+      acc[day] += 1;
+    } else {
+      acc[day] = 1;
+    }
+    return acc;
+  }, {});
   const tripsData = [
     ["Día", "Viajes"],
-    ["Lunes", 1],
-    ["Martes", 2],
-    ["Miércoles", 1],
-    ["Jueves", 0],
-    ["Viernes", 3],
+    ["Lunes", tripsPerDay["lunes"] || 0],
+    ["Martes", tripsPerDay["martes"] || 0],
+    ["Miércoles", tripsPerDay["miércoles"] || 0],
+    ["Jueves", tripsPerDay["jueves"] || 0],
+    ["Viernes", tripsPerDay["viernes"] || 0],
+  ];
+  const moneyRaised = requestedRides.reduce((acc, ride) => {
+    acc += Number(ride.money);
+    return acc;
+  }, 0);
+  const moneyPerDay = requestedRides.reduce((acc, ride) => {
+    const date = new Date(ride.date);
+    const day = date.toLocaleDateString("es-ES", { weekday: "long" });
+    if (acc[day]) {
+      acc[day] += Number(ride.money);
+    } else {
+      acc[day] = Number(ride.money);
+    }
+    return acc;
+  }, {});
+  const moneyPerDayData = [
+    ["Día", "Dinero gastado"],
+    ["Lunes", moneyPerDay["lunes"] || 0],
+    ["Martes", moneyPerDay["martes"] || 0],
+    ["Miércoles", moneyPerDay["miércoles"] || 0],
+    ["Jueves", moneyPerDay["jueves"] || 0],
+    ["Viernes", moneyPerDay["viernes"] || 0],
   ];
 
-  const moneyRaised = 58000;
-  0;
+  useEffect(() => {
+    const handleUser = async () => {
+      const id = localStorage.getItem("userId");
+      const data = await fetch("/api/getUser", {
+        method: "POST",
+        body: JSON.stringify({ id }),
+      });
+      const response = await data.json();
+      if (response.userData) {
+        setUser(response.userData);
+      }
+    };
+    handleUser();
+  }, []);
+
+  useEffect(() => {
+    const handleRides = async () => {
+      const data = await fetch("/api/getRides");
+      const response = await data.json();
+      if (response.userData) {
+        setViajes(response.userData);
+      }
+    };
+    handleRides();
+  }, []);
 
   const moneyData = [
-    ["Label", "Dinero recaudado"],
-    ["Dinero recaudado", moneyRaised],
-  ];
-
-  const moneyPerDayData = [
-    ["Día", "Dinero recaudado"],
-    ["Lunes", 100],
-    ["Martes", 200],
-    ["Miércoles", 300],
-    ["Jueves", 400],
-    ["Viernes", 500],
+    ["Label", "Dinero gastado"],
+    ["Dinero gastado", moneyRaised],
   ];
 
   const tripsOptions = {
-    title: "Viajes realizados en la semana",
+    title: "Días de la semana con más viajes:",
     hAxis: {
       title: "Viajes",
     },
@@ -42,7 +92,7 @@ export default function Stats() {
   };
 
   const moneyOptions = {
-    title: "Dinero recaudado histórico",
+    title: "Dinero histórico gastado",
     vAxis: {
       title: "Dinero",
     },
@@ -50,7 +100,7 @@ export default function Stats() {
   };
 
   const moneyPerDayOptions = {
-    title: "Dinero histórico recaudado por día de la semana",
+    title: "Dinero histórico gastado por día de la semana",
     hAxis: {
       title: "Día",
     },
